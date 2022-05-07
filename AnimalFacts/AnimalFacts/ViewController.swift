86ba854ad2d3cea3122ed2db3d5078c7173e041e
Marco Alarcon <alarconm96@gmail.com>
2022-05-07 00:26:30 -0400

    Add setLabels and convertToImage funcs

--- AnimalFacts/AnimalFacts/ViewController.swift
+++ AnimalFacts/AnimalFacts/ViewController.swift
@@ -25,6 +25,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    fileprivate func setLabels(){
+        let a = globalAnimal
+        
+        //set viewController labels to globalAnimal vars
+        lblAnimal.text = a.name
+        lblScientificName.text = a.scientificName
+        lblHabitat.text = a.habitat
+        lblWeight.text = a.weight
+        lblSize.text = a.size
+        txtDesc.text = a.desc
+        imgAnimal.image = convertToImage(urlString: a.img)
+    }
+    
+    //fetch image data from JSON
+    func convertToImage(urlString: String) -> UIImage {
+        //convert string to URL
+        let imgURL = URL(string: urlString)!
+        //call endpoint and receive the bytes
+        let imgDataBytes = try? Data(contentsOf: imgURL)
+        print(imgDataBytes ?? "Error image does not exist at URL \(imgURL)")
+        //convert bytes of data into image type
+        let img = UIImage(data: imgDataBytes!)
+        //return UIImage
+        return img!
+    }
 
 }
 
