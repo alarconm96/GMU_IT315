15cfdcd930214d3f66e159746c9aead27cbbc68a
Marco Alarcon <alarconm96@gmail.com>
2022-05-07 00:27:37 -0400

    Add setLabels method to viewDidLoad

--- AnimalFacts/AnimalFacts/ViewController.swift
+++ AnimalFacts/AnimalFacts/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setLabels()
     }
 
     fileprivate func setLabels(){
