9dd7f94df171e15970274bbe008dbeb963d0977f
Marco Alarcon <alarconm96@gmail.com>
2022-05-06 23:08:40 -0400

    Add new RootTableViewController class

---
+++ AnimalFacts/AnimalFacts/RootTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RootTableViewController.swift
+//  AnimalFacts
+//
+//  Created by Marco on 5/6/22.
+//
+
+import Foundation
+import UIKit
+
+class RootTableViewController : UITableViewController{
+    
+}
