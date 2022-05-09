0bd7fc66665d6cc5988dd58495200b76ff49dba2
Marco Alarcon <alarconm96@gmail.com>
2022-05-08 22:46:16 -0400

    Add DetailViewController file and view

---
+++ AnimalFacts/AnimalFacts/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  AnimalFacts
+//
+//  Created by Marco on 5/8/22.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
